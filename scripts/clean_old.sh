7cfb45c119b07ce34344ccfc31e7b88d770fa53a
Rashi Tiwari <117359006+RashiTiwari12@users.noreply.github.com>
2025-08-16 21:50:47 +0530

    Update clean_old.sh

--- scripts/clean_old.sh
+++ scripts/clean_old.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Clean the deployment folder
-rm -rf /home/ec2-user/ai-idea-generator/*
-
+# Delete old frontend and backend files safely
+sudo rm -rf /home/ec2-user/ai-idea-generator/frontend/*
+sudo rm -rf /home/ec2-user/ai-idea-generator/backend/*
